0d2a48a3bb815629ecf21125d752595de3e69734
FizzaSalman <159646126+FizzaSalman@users.noreply.github.com>
2024-03-27 14:04:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessednumber",
+        type: "number",
+        message: "Please guess a number between 1 - 7: ",
+    }
+]);
+if (answers.userGuessednumber === randomNumber) {
+    console.log("Congratulations!you guessed a right number.");
+}
+else {
+    console.log("You guessed wrong number");
+}
